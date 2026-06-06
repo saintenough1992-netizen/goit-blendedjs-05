ac19a52637a330ecb5fce54c46a4f5a16d37856a
Darman <saintenough1992@gmail.com>
2026-06-06 10:47:30 +0300

    data extraction

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import { refs } from './js/refs';
+import { addTask } from './js/tasks';
+
 /*
   Створи список справ.
   На сторінці є два інпути які має вводиться назва і текст задачі.
@@ -14,3 +17,4 @@
       <p>Текст</p>
   </li>
 */
+refs.form.addEventListener('submit', addTask);
